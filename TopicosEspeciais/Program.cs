4df9b9c95a2386eb8206b6cb73261b8b0142e449
viniciusnasc <vini.souza00@gmail.com>
2021-07-11 15:04:16 -0300

    Conclusao cap 7

--- TopicosEspeciais/Program.cs
+++ TopicosEspeciais/Program.cs
@@ -11,7 +11,8 @@ namespace TopicosEspeciais
                               "\nA1 para Expressão condicional ternaria" +
                               "\nA2 para funções para string(sem exercicio, apenas consulta)" +
                               "\nA3 para DateTime(sem exercicio, apenas consulta)" +
-                              "\nA4 para TimeSpan(sem exercicio, apenas consulta)");
+                              "\nA4 para TimeSpan(sem exercicio, apenas consulta)" +
+                              "\nA5 para Propriedades e operacoes DateTime(sem exercicio, apenas consulta)");
             string aula = Console.ReadLine().ToUpper();
 
             #region Expressão condicional ternaria
@@ -81,6 +82,88 @@ namespace TopicosEspeciais
                 TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);// Retorna 1.02:11:21:3210000, sendo 3210000, milisegundos
 
                 TimeSpan t6 = TimeSpan.FromDays(1.5); //O from retorna o valor em parametro como dia, hora, minutos, segundos ou milisegundos
+            }
+            #endregion
+
+            #region Propriedades e operacoes com DateTime
+            else if ( aula == "A5")
+            {
+                DateTime d1 = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+                Console.WriteLine("1) Date: " + d1.Date); // Traz apenas a data, o horario fica zerado - DateTime
+                Console.WriteLine("2) Day: " + d1.Day); // Vai trazer apenas o dia, em formato int
+                Console.WriteLine("3) DayOfWeek: " + d1.DayOfWeek); // Traz o dia da semana "Wednesday" - DayOfWeek
+                Console.WriteLine("4) DayOfYear: " + d1.DayOfYear);// Traz o dia do ano, 227 formato int
+                Console.WriteLine("5) Hour: " +d1.Hour); //Traz a hora, int
+                Console.WriteLine("6) Kind: " + d1.Kind); // Traz se é local ou universal - padrão: local - DateTime
+                Console.WriteLine("7) Milisecond: " + d1.Millisecond); // Int
+                Console.WriteLine("8) Minute: " + d1.Minute); // Int
+                Console.WriteLine("9) Month: " + d1.Month); // Int
+                Console.WriteLine("10) Seconds: " + d1.Second); // Int
+                Console.WriteLine("11) Ticks: " + d1.Ticks); // Long
+                Console.WriteLine("12) TimeOfDay: " + d1.TimeOfDay); //Apenas o horario - TimeSpan
+                Console.WriteLine("13) Year: " + d1.Year); // Int
+
+                // Formatação
+                DateTime d2 = new DateTime(2001, 8, 15, 13, 45, 58);
+
+                Console.WriteLine(d2.ToLongDateString()); // quarta-feira, 15 de agosto de 2001
+                Console.WriteLine(d2.ToLongTimeString()); // 13:45:58
+                Console.WriteLine(d2.ToShortDateString()); // 15/08/2001
+                Console.WriteLine(d2.ToShortTimeString()); // 13:45
+                Console.WriteLine(d2.ToString()); // 15/08/2001 13:45:58
+                Console.WriteLine(d2.ToString("yyyy-MM-dd HH:mm:ss")); // 2001-08-15 13:45:58
+                Console.WriteLine(d2.ToString("yyyy-MM-dd HH:mm:ss.fff")); // 2001-08-15 13:45:58.000
+
+                // Operações
+                DateTime d3 = new DateTime(2001, 8, 15, 13, 45, 58);
+
+                d3 = d3.AddDays(7);
+                d3 = d3.AddHours(2);
+                d3 = d3.AddMilliseconds(24);
+                d3 = d3.AddMinutes(3);
+                d3 = d3.AddMonths(13);
+                d3 = d3.AddSeconds(324);
+                d3 = d3.AddTicks(2444);
+                d3 = d3.AddYears(3);
+
+                TimeSpan t1 = d3.Subtract(d2); 
+                d3 = d3.Subtract(t1);
+                d3 = d3.Add(t1);
+            }
+            #endregion
+
+            #region Propriedades e operacoes com TimeSpan
+            else if (aula == "A6")
+            {
+                TimeSpan t1 = TimeSpan.MaxValue; // Valor máximo possivel de um TimeSpan
+                TimeSpan t2 = TimeSpan.MinValue; // Valor mínimo - pode ter valor negativo
+                TimeSpan t3 = TimeSpan.Zero; // Valor zerado
+
+                TimeSpan t4 = new TimeSpan(2, 3, 5, 7, 11);
+
+                int dias = t4.Days; // 2
+                int horas = t4.Hours; // 3
+                int minutos = t4.Minutes; // 5
+                int segundos = t4.Seconds; // 7
+                int milisegundos = t4.Milliseconds; // 011
+                long ticks = t4.Ticks;
+
+                double totalDias = t4.TotalDays; // Vai trazer numero quebrado, totalizando o total de dias junto as horas, min, sec, milisec
+                double totalHoras = t4.TotalHours;
+                double totalMinutos = t4.TotalMinutes;
+                double totalSegundos = t4.TotalSeconds;
+                double totalMilisegundos = t4.TotalMilliseconds;
+
+                // Operacoes
+
+                TimeSpan t5 = new TimeSpan(1, 30, 10);
+                TimeSpan t6 = new TimeSpan(0, 10, 5);
+
+                TimeSpan soma = t5.Add(t6);
+                TimeSpan subtracao = t5.Subtract(t6);
+                TimeSpan multiplicacao = t5.Multiply(2);
+                TimeSpan divisao = t5.Divide(2);
+
             }
             #endregion
         }
